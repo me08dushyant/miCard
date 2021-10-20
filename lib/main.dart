b192d06bc051ec58a3e345d4e70eb5bf24e9b0ae
FlutLab.io <>
2021-10-20 06:27:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,11 @@ class MyHomePage extends StatelessWidget {
               height: 100,
               color: Colors.red,
             ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
           ],
         ),
       ),
