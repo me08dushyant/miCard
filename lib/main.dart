25a1274b90732ebd099d9a21fe131fdd0b25dd0d
FlutLab.io <>
2021-10-22 05:28:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,15 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: ListTile(leading: Icon(Icons.arrow_drop_down_circle), title: Text('me08dushyant@gmail.com')),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
